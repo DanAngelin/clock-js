208a187282942ec739ed6fbbfdec71d075e0ff73
DanAngelin <dan.angelin@hotmail.com>
2021-11-27 17:29:48 +0200

    add clock js UTC+0

--- js/app.js
+++ js/app.js
@@ -4,21 +4,33 @@ function clockJs() {
     const clock = new Date();
 
     let date = clock.getDate();
+    let dateUTC = clock.getUTCDate();
 
     let day = clock.getDay();
+    let dayUTC = clock.getUTCDay();
     let daysArray = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
     day = daysArray[day];
+    dayUTC = daysArray[dayUTC];
 
     let month = clock.getMonth();
+    let monthUTC = clock.getUTCMonth();
     let monthsArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     month = monthsArray[month];
+    monthUTC = monthsArray[monthUTC]
     
     let year = clock.getFullYear();
+    let yearUTC = clock.getUTCFullYear();
 
     let hour = clock.getHours();
     let minute = clock.getMinutes();
     let second = clock.getSeconds();
 
+    let hourUTC = clock.getUTCHours();
+    let minuteUTC = clock.getUTCMinutes();
+    let secondUTC = clock.getUTCSeconds();
+
     document.querySelector('#date').innerText = `${day} ${date} ${month} ${year}` ;
     document.querySelector('#time').innerText = `${hour} : ${minute} : ${second}` ;
+    document.querySelector('#dateutc').innerText = `${dayUTC} ${dateUTC} ${monthUTC} ${yearUTC}` ;
+    document.querySelector('#timeutc').innerText = `${hourUTC} : ${minuteUTC} : ${secondUTC} UTC` ;
 }
